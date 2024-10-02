4b794a8fbb954c4d0f4670c3efd4c2c885f5cacb
cheatercodding <amabazakeremabaza@gmail.com>
2024-10-02 11:47:36 +0300

    alan adlarını tanımlama

--- newproje2.py
+++ newproje2.py
@@ -2,3 +2,12 @@
 def formatDomain(domain):
     turkish_lower_map = str.maketrans("IİŞĞÜÇÖ", "ıişğüçö")
     return domain.translate(turkish_lower_map).lower()
+
+# Alan adlarını liste olarak tanımlama
+
+domains = [
+    "TEKNOLOJI.COM", "teknoloji.com", "TeKnoLoJi.cOm", "KITAPLIK.COM", "kitaplik.com",
+    "KiTaPlIk.CoM", "EĞİTİM.COM", "egitim.com", "EgItIm.cOm", "SAĞLIK.COM",
+    "saglik.com", "SaGlIk.CoM", "YEMEKTARİFİ.COM", "yemektarifi.com", "YeMeKTaRiFi.CoM",
+    "OYUNLAR.COM", "oyunlar.com", "OyUnLaR.cOm", "SİTE.COM", "site.com"
+]
